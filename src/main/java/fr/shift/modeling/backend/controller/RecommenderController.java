ecd1cc3997a3f36c818ee359d37ade9ec82543c6
Maxime Savary-Leblanc <maxime.savaryleblanc@gmail.com>
2022-12-08 21:23:14 +0100

    Clean code

--- src/main/java/fr/shift/modeling/backend/controller/RecommenderController.java
+++ src/main/java/fr/shift/modeling/backend/controller/RecommenderController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.async.DeferredResult;
 import reactor.util.retry.Retry;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.time.Duration;
 import java.util.*;
